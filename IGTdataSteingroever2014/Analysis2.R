360eb13eb501c3ce66df72f20f9cc631f4831300
Parth Shirish Nandedkar <54970210+parthnan@users.noreply.github.com>
2019-11-11 17:04:26 +0900

    Add files via upload

---
+++ IGTdataSteingroever2014/Analysis2.R
@@ -0,0 +1,56 @@
+# 研究事例（論文の第一著者名）の抽出（100試行のデータ）
+author <- unique(index_100[,2])
+
+# 第一番目の研究事例（Horstmann）のデータ抽出
+studyName <- author[1]
+subIndex <- as.numeric(index_100[index_100[,2]==studyName,1])
+choice_data <- choice_100[subIndex,]
+win_data <- wi_100[subIndex,]
+lose_data <- lo_100[subIndex,]
+reward_data <- win_data+lose_data
+cumReward <- t(apply(reward_data,1,cumsum))
+
+# 第一番目の実験参加者の結果
+## 累積報酬
+cdata <- cumReward[2,]
+if(cdata[100]<0){
+  gcol="Red"
+}else{
+  gcol="Black"
+}
+plot(cdata,type="l",col=gcol,ylim=c(-3000,3000),xlab="Trials",ylab="Cumulative reward")
+
+# 全ての実験参加者の結果
+## 累積報酬
+par(mfrow=c(4,4))
+for(i in 33:48){
+  cdata <- cumReward[i,]
+  if(cdata[100]<0){
+    gcol="Red"
+  }else{
+    gcol="Black"
+  }
+  plot(cdata,type="l",col=gcol,ylim=c(-3000,3000),xlab="Trials",ylab="Cumulative reward")
+}
+
+pca_result <- prcomp(cumReward,scale=TRUE)
+summary(pca_result)
+pca_result$rotation
+
+
+pca_result$x
+
+plot(pca_result$rotation[,2],type="l",xlab="Trials")
+
+
+plot(pca_result$x[,1],pca_result$x[,2])
+
+gcoll <- numeric(162)
+for(i in 1:162){
+  cdata <- cumReward[i,]
+  if(cdata[100]<0){
+    gcoll[i]="Red"
+  }else{
+   gcoll[i]="Black"
+}
+}
---
+++ IGTdataSteingroever2014/Analysis3-1.R
@@ -0,0 +1,176 @@
+index_100 <- read.csv("index_100.csv")
+choice_100 <- read.csv("choice_100.csv")
+wi_100 <- read.csv("wi_100.csv")
+lo_100 <- read.csv("lo_100.csv")
+
+
+# 研究事例（論文の第一著者名）の抽出（100試行のデータ）
+author <- unique(index_100[,2])
+
+# 第一番目の研究事例（Horstmann）のデータ抽出
+studyName <- author[1]
+subIndex <- as.numeric(index_100[index_100[,2]==studyName,1])
+choice_data <- choice_100[subIndex,]
+win_data <- wi_100[subIndex,]
+lose_data <- lo_100[subIndex,]
+
+# 第一番目の実験参加者の結果
+
+as.vector(win_data["Subj_1",])+as.vector(lose_data["Subj_1",])
+
+subID=1
+valscale <- 0.01 #得失・損失の額のスケーリング
+
+# 価値判断：パラメータw
+valence <- function(w){
+  return(as.numeric(valscale*w*win_data[subID,])+as.numeric(valscale*(1-w)*lose_data[subID,]))
+}
+
+# 期待価値の更新：パラメータa
+expected_valence <- function(a,t,val,expval){
+  return(expval[t-1]+a*(val[t]-expval[t-1]))
+}
+
+# 全てのカードの山の期待価値の更新
+expected_valence_all <- function(w,a){
+  EE <-NULL
+  for (j in 1:4) {
+    E <- NULL
+    for (t in 1:100) {
+      # 1回目の更新
+      if (t==1) {
+        if (choice_data[subID,1]==j) {
+          E <-valence(w)[1]
+        }else{
+          E <- 0
+        }
+      }else{
+        # 2回目以降の更新
+        if (choice_data[subID,t]==j) {
+          EV <- expected_valence(a,t,valence(w),E)
+          E <- c(E,EV)
+        }else{
+          E <- c(E,E[t-1])
+        }
+      }
+    }
+    EE <- c(EE,list(E))
+  }
+  return(EE)
+}
+
+# 選択の一貫性：パラメータc
+theta <- function(c){
+  return(((1:100)/10)^c)
+}
+
+# カードの山の選択確率
+select_deck <- function(w,a,c){
+  EA <-expected_valence_all(w,a)
+  P1 <- exp(theta(c)*EA[[1]])
+  P2 <- exp(theta(c)*EA[[2]])
+  P3 <- exp(theta(c)*EA[[3]])
+  P4 <- exp(theta(c)*EA[[4]])
+  
+  PP <- P1+P2+P3+P4
+  
+  PP1 <- P1/PP
+  PP2 <- P2/PP
+  PP3 <- P3/PP
+  PP4 <- P4/PP
+  
+  return(list(PP1,PP2,PP3,PP4))
+}
+
+# 全選択の対数尤度
+LL <- function(pp){
+  D <- select_deck(pp[1],pp[2],pp[3])
+  S <- 0
+  for (t in 1:100) {
+    for (j in 1:4) {
+      if (choice_data[subID,t]==j) {
+        S <-log(D[[j]][t])+S
+      }
+    }
+  }
+  return(-S)
+}
+
+system.time(
+result <- optim(par=c(0.5,0.5,0.01),fn=LL,method = "L-BFGS-B",lower = c(0,0,-2),upper = c(1,1,2) )
+)
+
+# Expectancy Valence modelの当てはめ
+expected_valence <- function(a,t,val,expval){
+  return(expval[t-1]+a*(val[t]-expval[t-1]))
+}
+
+theta <- function(c){
+  #return(((1:100)/10)^c)
+  return((3^c)-1)
+}
+
+
+LL <- function(subID){
+  function(pp){
+    w <- pp[1]
+    a <- pp[2]
+    c <- pp[3]
+    valw <- as.numeric(valscale*w*win_data[subID,])+as.numeric(valscale*(1-w)*lose_data[subID,])
+    EE <- NULL
+    for(j in 1:4){
+      E <- NULL
+      for (t in 1:100) {
+        # 1回目の更新
+        if (t==1) {
+          if (choice_data[subID,1]==j) {
+            E <- valw[1]
+          }else{
+            E <- 0
+          }
+        }else{
+          # 2回目以降の更新
+          if (choice_data[subID,t]==j) {
+            EV <- expected_valence(a,t,valw,E)
+            E <- c(E,EV)
+          }else{
+            E <- c(E,E[t-1])
+          }
+        }
+      }
+      EE <- c(EE, list(E))
+    }
+    
+    P1 <- exp(theta(c)*EE[[1]])
+    P2 <- exp(theta(c)*EE[[2]])
+    P3 <- exp(theta(c)*EE[[3]])
+    P4 <- exp(theta(c)*EE[[4]])
+    PP <- P1+P2+P3+P4
+    PP1 <- P1/PP
+    PP2 <- P2/PP
+    PP3 <- P3/PP
+    PP4 <- P4/PP
+    
+    D <- list(PP1,PP2,PP3,PP4)
+    S <- 0
+    for(t in 1:100){
+      for(j in 1:4){
+        if(choice_data[subID,t] == j){
+          S <- S + log(D[[j]][t])
+        }
+      }
+    }
+    return(S)
+  }
+}
+
+system.time(
+  result <- optim(par=c(0.5,0.5,0.01),fn=LL(1),method = "L-BFGS-B",lower = c(0,0,-2),upper = c(1,1,2),control=list(fnscale=-1))
+)
+
+Esti <- matrix(0, 162, 3)
+for(i in 1:162){
+  mle <- optim(par=c(0.5,0.5,0.01), fn=LL(i), lower=c(0.001,0.001,-1), upper=c(0.999,0.999,1), method="L-BFGS-B", control=list(fnscale=-1))
+  Esti[i,] <- mle$par
+}
+
